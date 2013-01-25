a3b0b841e29ab998e98b7255774b4a1f22f77612
Philipp Küng <git@philippkueng.ch>
2013-01-25 07:21:14 +0100

    extended app with views, static folder and more complete express configuration section

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
-var express = require('express');
+var express = require('express'),
+    routes = require('./routes'),
+    http = require('http'),
+    path = require('path');
 
-var app = express(express.logger());
+var app = express();
 
-app.get('/', function(request, response) {
-  response.send('Hello World from Zurichtechtalks!');
+app.configure(function(){
+    app.set('port', process.env.PORT || 5000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.configure('development', function(){
+    app.use(express.errorHandler());
+});
+
+
+/*
+ * ROUTES
+ *
+ */
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('Server listening on port ' + app.get('port'));
 });
