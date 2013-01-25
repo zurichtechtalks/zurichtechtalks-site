cf5725b9e5eacc1f47f9bc16c3585031f7f0c420
Philipp Küng <git@philippkueng.ch>
2013-01-25 21:11:49 +0100

    unified tab-spacing to 4 spaces

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,10 @@
 ;(function ($, window, undefined) {
-  'use strict';
+    'use strict';
 
-  var $doc = $(document),
-      Modernizr = window.Modernizr;
+    var $doc = $(document),
+        Modernizr = window.Modernizr;
 
-  $(document).ready(function() {
+$(document).ready(function() {
     $.fn.foundationAlerts           ? $doc.foundationAlerts() : null;
     $.fn.foundationButtons          ? $doc.foundationButtons() : null;
     $.fn.foundationAccordion        ? $doc.foundationAccordion() : null;
@@ -18,21 +18,21 @@
     $.fn.foundationClearing         ? $doc.foundationClearing() : null;
 
     $.fn.placeholder                ? $('input, textarea').placeholder() : null;
-  });
+});
 
-  // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
-  // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
-  // $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'both'});
-  // $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'both'});
-  // $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'both'});
+// UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
+// $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
+// $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'both'});
+// $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'both'});
+// $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'both'});
 
-  // Hide address bar on mobile devices (except if #hash present, so we don't mess up deep linking).
-  if (Modernizr.touch && !window.location.hash) {
+// Hide address bar on mobile devices (except if #hash present, so we don't mess up deep linking).
+if (Modernizr.touch && !window.location.hash) {
     $(window).load(function () {
-      setTimeout(function () {
-        window.scrollTo(0, 1);
-      }, 0);
+        setTimeout(function () {
+            window.scrollTo(0, 1);
+        }, 0);
     });
-  }
+}
 
 })(jQuery, this);
