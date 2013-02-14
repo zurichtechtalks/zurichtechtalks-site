0e358827c032396e9a948ab996a1d5355ad351fd
Philipp Küng <git@philippkueng.ch>
2013-02-14 20:19:06 +0100

    removed ember files; fixed tests

--- test/test.post.js
+++ test/test.post.js
@@ -8,13 +8,13 @@ describe('Posts', function(){
         request
             .get(root_url + '/posts')
             .end(function(result){
-                JSON.parse(result.text).length.should.equal(20);
+                JSON.parse(result.text).length.should.equal(3);
                 done();
             });
     });
-    it('/posts/0 should return a single post', function(done){
+    it('/posts/41099563070 should return a single post', function(done){
         request
-            .get(root_url + '/posts/0')
+            .get(root_url + '/posts/41347982488')
             .end(function(result){
                 JSON.parse(result.text).title.should.equal('Cloud Foundry Bootcamp @zhgeeks');
                 JSON.parse(result.text).tags.length.should.equal(14);
